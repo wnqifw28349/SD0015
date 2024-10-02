6bf9bc40c03806589ab98a924e4090046e393e28
wnqifw28349 <cshemar1998@gmail.com>
2024-10-02 16:07:59 +0100

    TechEd Technical Challenge

---
+++ script.js
@@ -0,0 +1,9 @@
+let username = prompt("Pick a number between 1 and 5.");
+
+if (username == "2") {
+  document.body.style.backgroundColor = "white";
+  alert("Welcome to my page.");
+} else {
+  document.body.style.backgroundColor = "black";
+  alert("Whoops, try again.");
+}
